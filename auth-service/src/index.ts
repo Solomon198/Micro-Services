4c6c37ca7d6aade69c6903f95ba1af9c78331a38
Solomon Yunana <solomonyunana95@gmail.com>
2023-08-02 12:55:27 +0100

    added tests for sigin and signup, configured tests

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -1,32 +1,5 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
-import { currentUserRouter } from "./routes/current-user";
-import { signupRouter } from "./routes/signup";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { ErrorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-const app = express();
-app.set("trust proxy", true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-app.use(currentUserRouter);
-app.use(signupRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.all("*", async () => {
-  throw new NotFoundError();
-});
-app.use(ErrorHandler);
+import { app } from "./app";
 
 const startApplication = async () => {
   if (!process.env.JWT_KEY) {
