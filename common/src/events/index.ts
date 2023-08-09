02173f8c8551e214d116ec34dd8e1476d55f51d3
Solomon Yunana <solomonyunana95@gmail.com>
2023-08-09 14:16:48 +0100

    udated package

--- common/src/events/index.ts
+++ common/src/events/index.ts
@@ -3,3 +3,4 @@ export { Listener } from "./base-listener";
 export { TicketCreatedEvent } from "./ticket-created-event";
 export { TicketUpdatedEvent } from "./ticket-updated-event";
 export { Subjects } from "./subjects";
+export { OrderStatus } from "./types/order-status";
