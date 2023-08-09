918b0c917c13614ae7ed76862aceb8d20cb3a4fd
Solomon Yunana <solomonyunana95@gmail.com>
2023-08-09 17:41:37 +0100

    added orders events to common

--- common/src/events/index.ts
+++ common/src/events/index.ts
@@ -2,5 +2,7 @@ export { Publisher } from "./base-publisher";
 export { Listener } from "./base-listener";
 export { TicketCreatedEvent } from "./ticket-created-event";
 export { TicketUpdatedEvent } from "./ticket-updated-event";
+export { OrderCancelledEvent } from "./order-cancelled-event";
+export { OrderCreatedEvent } from "./order-created-event";
 export { Subjects } from "./subjects";
 export { OrderStatus } from "./types/order-status";
